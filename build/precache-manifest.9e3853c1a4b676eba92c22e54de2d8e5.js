f91fdb2dca82c0915871c2020000d88f2cf970ec
Austin <a.rossfortson@gmail.com>
2019-07-17 17:47:29 -0400

    added build for deployment

---
+++ build/precache-manifest.9e3853c1a4b676eba92c22e54de2d8e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ce4938e38bfe195b829b7461c0002e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "908238f75b0c70d90edc",
+    "url": "/static/css/main.16189d5e.chunk.css"
+  },
+  {
+    "revision": "383a3b71ba0a5510eb79",
+    "url": "/static/js/2.b9e231f4.chunk.js"
+  },
+  {
+    "revision": "908238f75b0c70d90edc",
+    "url": "/static/js/main.fc89d234.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
