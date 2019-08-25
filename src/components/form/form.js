2d0a5d5b86fcbb2956de62b27ac84ef0d7cf2006
Austin <a.rossfortson@gmail.com>
2019-08-24 20:35:44 -0400

    updated github security alert

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -2,10 +2,11 @@ import React from 'react';
 import './form.css'
 
 class Form extends React.Component {
+  
     render () {
-    return (
+      return (
         <div>
-        <form name="contact" id="contactForm" method="POST" data-netlify="true" data-netlify-recaptcha="true">
+        <form name="contact" id="contactForm" method="POST" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <label>Name</label>
         <input name="name" type="text" id="name"  placeholder="Enter Your Name Here"></input>
@@ -16,7 +17,6 @@ class Form extends React.Component {
         <label>Message</label>
         <textarea name ="message" type="message" id="message" placeholder="Enter Your Message Here"></textarea>
 
-        <div id="recaptcha" data-netlify-recaptcha="true"></div>
         <input id="submit" name="submit" type="submit"></input>
       </form>    
     </div>
